6d1f1b960213e7c91869d468b0c197927826b6d6
twx <3073239759@qq.com>
2020-11-25 21:19:10 +0800

    commit

--- src/main/java/edu/nf/users/entity/Users.java
+++ src/main/java/edu/nf/users/entity/Users.java
@@ -1,5 +1,16 @@
 package edu.nf.users.entity;
 
-public class Users {
+import jdk.nashorn.internal.objects.annotations.Constructor;
+import lombok.Data;
 
+@Data
+public class Users {
+    private Integer id;
+    private String name;
+    private String pwd;
+    private Integer age;
+    private String sex;
+    private String tel;
+    private String email;
+    private String address;
 }
--- src/test/java/edu/nf/users/test/UserTest.java
+++ src/test/java/edu/nf/users/test/UserTest.java
@@ -1,4 +1,11 @@
 package edu.nf.users.test;
 
+import edu.nf.users.service.UserService;
+import org.junit.Test;
+
 public class UserTest {
+    @Test
+    public void testlistUser(){
+        System.out.println(new UserService().usersList());
+    }
 }
